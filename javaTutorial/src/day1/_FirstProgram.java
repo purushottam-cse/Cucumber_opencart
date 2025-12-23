5b5769c924a733016246c64b0b02901f65d2782a
purus <purus@DESKTOP-PD2GSFH>
2025-12-23 19:19:29 +0530

    second

--- javaTutorial/src/day1/_FirstProgram.java
+++ javaTutorial/src/day1/_FirstProgram.java
@@ -5,6 +5,7 @@ public class _FirstProgram
 	public static void main(String args[]) {
 		System.out.println("I am automation test engineer");
 		// print method
+		System.out.println("hii");
 	}
 
 }
